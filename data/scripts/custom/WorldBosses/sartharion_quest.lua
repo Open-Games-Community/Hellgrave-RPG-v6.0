a1c3d3f790aa8f61248009f3e078837eac5af2fa
Alexv45 <89811188+Alexv45@users.noreply.github.com>
2023-04-16 08:37:43 +0200

    #Fixed WorldBoss
    Fixed an error saying "sendTextMessage" if a player try to get again the traces of a worldboss.

--- data/scripts/custom/WorldBosses/sartharion_quest.lua
+++ data/scripts/custom/WorldBosses/sartharion_quest.lua
@@ -6,7 +6,7 @@ function firstWorldBoss.onStepIn(player, item, fromPosition)
         player:sendTextMessage(MESSAGE_INFO_DESCR, "You have found some rare Traces, check your questlog for additional information.")
         return true
     elseif player:getStorageValue(Storage.WorldBosses.Unlock.Sartharion) == 1 or player:getStorageValue(Storage.WorldBosses.Unlock.Sartharion) == 2 then
-        player:sendTextMessage(MESSAGE_STATUS_WARNING, "You already have discovered the rares traces.")
+        return false
     else
         player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found some Traces, but you didn't finish the Secret Quest and you dont know how to find a rare monster.")
     end
